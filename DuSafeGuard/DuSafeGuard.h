68618351e0a95c5d699a500face50b629c35e9a9
Programmer.Du <zengyun.programmer@gmail.com>
2018-07-12 00:18:34 +0800

    add project

---
+++ DuSafeGuard/DuSafeGuard.h
@@ -0,0 +1,19 @@
+//
+//  DuSafeGuard.h
+//  DuSafeGuard
+//
+//  Created by Du on 12/07/2018.
+//  Copyright © 2018 SnowWolf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DuSafeGuard.
+FOUNDATION_EXPORT double DuSafeGuardVersionNumber;
+
+//! Project version string for DuSafeGuard.
+FOUNDATION_EXPORT const unsigned char DuSafeGuardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DuSafeGuard/PublicHeader.h>
+
+
